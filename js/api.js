87584951b7e18563df9959233dbef878ea255af7
armejbha <abdurrahim181878@gmil.com>
2022-02-25 16:30:36 +0600

    search player api

--- js/api.js
+++ js/api.js
@@ -1,5 +1,29 @@
-const loadData = () => {
-    fetch('https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=Danny%20Welbeck')
+const loadData = async () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    const url = (`https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchText}`);
+    fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayData(data.player[0]))
+}
+const displayData = player => {
+    const playerArea = document.getElementById('player-area');
+    playerArea.innerHTML = "";
+    const div = document.createElement('div');
+    div.className = 'row border border-primary p-2';
+    div.innerHTML = `
+    <div class="col-lg-4">
+        <img class="w-100" src="${player.strThumb}" alt="">
+    </div>
+    <div class="col-lg-8">
+        <p>Name:${player.strPlayer}</p>
+        <p>Date Of Birth:${player.dateBorn}</p>
+        <p>Nationality:${player.strNationality}</p>
+        <p>Position:${player.strPosition}</p>
+        <p>Team:${player.strTeam2}</p>
+        <p>Weight:${player.strWeight}</p>
+    </div>
+    `
+    playerArea.appendChild(div);
+
 }
